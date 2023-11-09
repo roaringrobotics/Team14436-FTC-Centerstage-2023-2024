4241d552d0fb0c9dc21b8d6afbca4974d9746799
timmyjr11 <timmyytjr@gmail.com>
2023-11-08 17:30:44 -0700

    Added back in "yes.java" so programming can continue

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/yes.java
@@ -0,0 +1,143 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class yes extends LinearOpMode {
+    //Hardware and variables
+    DcMotor testMotor;
+
+    DcMotor testMotor1;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //All movement happen
+        testMotor = hardwareMap.dcMotor.get("testMotor");
+        testMotor1 = hardwareMap.dcMotor.get("testMotor1");
+
+        testMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        testMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        ElapsedTime run = new ElapsedTime();
+
+        waitForStart();
+        while (run.time() < 200) {
+            testMotor1.setPower(1);
+
+        }
+        while (run.time() < 550) {
+            testMotor.setPower(1);
+            testMotor1.setPower(1);
+            // Score action 1
+        }
+        while (run.time() < 550) {
+            testMotor1.setPower(-1);
+            testMotor.setPower(-1);
+        }
+        while (run.time() < 200) {
+            testMotor1.setPower(1);
+        }
+        while (run.time() < 255) {
+            testMotor.setPower(1);
+            testMotor1.setPower(1);
+        }
+        while (run.time() < 200) {
+            testMotor1.setPower(1);
+        }
+        while (run.time() < 120) {
+            testMotor.setPower(1);
+            testMotor1.setPower(1);
+        }
+        // Intake of pixels (2).1
+        while (run.time() < 120) {
+            testMotor.setPower(-1);
+            testMotor1.setPower(-1);
+        }
+        while (run.time() < 200) {
+            testMotor1.setPower(-1);
+        }
+        while (run.time() < 255) {
+            testMotor.setPower(1);
+            testMotor1.setPower(1);
+        }
+        while (run.time() < 200) {
+            testMotor1.setPower(-1);
+        }
+
+        while (run.time() < 550) {
+            testMotor.setPower(1);
+            testMotor1.setPower(1);
+            // Score action 2
+        }
+        while (run.time() < 550) {
+            testMotor1.setPower(-1);
+            testMotor.setPower(-1);
+        }
+        while (run.time() < 200) {
+            testMotor1.setPower(1);
+        }
+        while (run.time() < 255) {
+            testMotor.setPower(1);
+            testMotor1.setPower(1);
+        }
+        while (run.time() < 200) {
+            testMotor1.setPower(1);
+        }
+        while (run.time() < 120) {
+            testMotor.setPower(1);
+            testMotor1.setPower(1);
+        }
+        // Intake of pixels (2).2  //*
+        while (run.time() < 120) {
+            testMotor.setPower(-1);
+            testMotor1.setPower(-1);
+        }
+        while (run.time() < 200) {
+            testMotor1.setPower(-1);
+        }
+
+        while (run.time() < 200) {
+            testMotor1.setPower(1);
+
+        }
+        while (run.time() < 550) {
+            testMotor.setPower(1);
+            testMotor1.setPower(1);
+            // Score action 1
+        }
+        while (run.time() < 550) {
+            testMotor1.setPower(-1);
+            testMotor.setPower(-1);
+        }
+        while (run.time() < 200) {
+            testMotor1.setPower(1);
+        }
+        while (run.time() < 255) {
+            testMotor.setPower(1);
+            testMotor1.setPower(1);
+        }
+        while (run.time() < 200) {
+            testMotor1.setPower(1);
+        }
+        while (run.time() < 120) {
+            testMotor.setPower(1);
+            testMotor1.setPower(1);
+        }
+        // Intake of pixels (2).1  //*
+        while (run.time() < 120) {
+            testMotor.setPower(-1);
+            testMotor1.setPower(-1);
+        }
+        while (run.time() < 200) {
+            testMotor1.setPower(-1);
+        }
+        while (run.time() < 255) {
+            testMotor.setPower(1);
+            testMotor1.setPower(1);
+        }
+    }
+
+}

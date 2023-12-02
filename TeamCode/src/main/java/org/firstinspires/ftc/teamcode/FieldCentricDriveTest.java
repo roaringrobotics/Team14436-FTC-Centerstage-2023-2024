43760c4dc40bb351c8d6d30c7191763918b86e31
timmyjr11 <timmyytjr@gmail.com>
2023-12-02 13:54:11 -0700

    Sending class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDriveTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.BNO055IMUImpl;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
+
+public class FieldCentricDriveTest extends LinearOpMode {
+
+    DcMotorEx frontLeft, frontRight, backRight, backLeft, blueIntake, Bluelift;
+    BNO055IMU imu;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+        Bluelift = hardwareMap.get(DcMotorEx.class, " Bluelift");
+        blueIntake = hardwareMap.get(DcMotorEx.class, " blueIntake");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+
+        waitForStart();
+
+        while(opModeIsActive() && !opModeIsActive()) {
+            telemetry.addData("Degree: ", imu.getAngularOrientation());
+            telemetry.addData("Parameters: ", imu.getParameters());
+            telemetry.update();
+        }
+
+    }
+}

745c90273152c45128b83ac0871bc9c3332a3ef8
roaringrobotics <roaringrobotics14436@gmail.com>
2023-12-01 20:26:14 -0700

    Code 2.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
@@ -1,94 +1,68 @@
-package org.firstinspires.ftc.teamcode;// Import the necessary libraries
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 
-// Define the name of the op mode
-@TeleOp(name = "Mecanum Robot", group = "Linear Opmode")
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-// Extend the LinearOpMode class
- public class MecanumDriveTrain extends LinearOpMode {
+@TeleOp
+public class MecanumDriveTrain extends LinearOpMode{
+    DcMotor backLeftMotor;
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backRightMotor;
+    DcMotor Bluelift;
+    DcMotor blueIntake;
 
-    // Declare the motor objects
-    private DcMotor frontLeftMotor;
-    private DcMotor frontRightMotor;
-    private DcMotor backLeftMotor;
-    private DcMotor backRightMotor;
-    private static final String FRONT_LEFT_MOTOR = "frontLeftMotor";
-    private static final String FRONT_RIGHT_MOTOR = "frontRightMotor";
-    private static final String BACK_LEFT_MOTOR = "backLeftMotor";
-    private static final String BACK_RIGHT_MOTOR = "backRightMotor";
 
-    private static final DcMotorSimple.Direction FRONT_LEFT_DIRECTION = DcMotorSimple.Direction.FORWARD;
-    private static final DcMotorSimple.Direction FRONT_RIGHT_DIRECTION = DcMotorSimple.Direction.REVERSE;
-    private static final DcMotorSimple.Direction BACK_LEFT_DIRECTION = DcMotorSimple.Direction.FORWARD;
-    private static final DcMotorSimple.Direction BACK_RIGHT_DIRECTION = DcMotorSimple.Direction.REVERSE;
+@Override
+ public void runOpMode() throws InterruptedException{
+blueIntake = hardwareMap.get(DcMotor.class,"blueIntake");
+Bluelift = hardwareMap.get(DcMotor.class,"Bluelift");
+backRightMotor = hardwareMap.get(DcMotor.class,"backRightMotor");
+backLeftMotor= hardwareMap.get(DcMotor.class,"backLeftMotor");
+frontLeftMotor= hardwareMap.get(DcMotor.class,"frontLeftMotor");
+frontRightMotor= hardwareMap.get(DcMotor.class,"frontRightMotor");
 
-    @Override
-    public void runOpMode() {
+frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        // Initialize the motors
-        frontLeftMotor = hardwareMap.get(DcMotor.class, FRONT_LEFT_MOTOR);
-        frontRightMotor = hardwareMap.get(DcMotor.class, FRONT_RIGHT_MOTOR);
-        backLeftMotor = hardwareMap.get(DcMotor.class, BACK_LEFT_MOTOR);
-        backRightMotor = hardwareMap.get(DcMotor.class, BACK_RIGHT_MOTOR);
+    IMU imu = hardwareMap.get(IMU.class,"imu");
 
-        // Set the motor directions
-        frontLeftMotor.setDirection(FRONT_LEFT_DIRECTION);
-        frontRightMotor.setDirection(FRONT_RIGHT_DIRECTION);
-        backLeftMotor.setDirection(BACK_LEFT_DIRECTION);
-        backRightMotor.setDirection(BACK_RIGHT_DIRECTION);
+    IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+            RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+            RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
+    imu.initialize(parameters);
 
-        // Set the motor modes
-        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        // Wait for the start button to be pressed
-        waitForStart();
+waitForStart();
+while (opModeIsActive()){
+    double y = -gamepad1.left_stick_y;
+    double x = gamepad1.left_stick_x;
+    double rx = gamepad1.right_stick_x;
 
-        // Loop until the op mode is stopped
-        while (opModeIsActive()) {
+    double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
-          if(gamepad1.a){
+    double rotx = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+    double roty = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+    rotx = rotx * 1.1;
 
-          }
+    double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
 
-            // Get the joystick values
-            double x = -gamepad1.left_stick_x; // Strafe axis
-            double y = -gamepad1.left_stick_y; // Forward axis
-            double z = -gamepad1.right_stick_x; // Rotate axis
+    double frontLeftMotorPower = (rotY + rotX + rx) / denominator;
+    double frontRightMotorPower = (rotY - rotX - rx) / denominator;
+    double backLeftMotorPower = (rotY - rotX + rx) / denominator;
+    double backRightMotorPower = (rotY + rotX - rx) / denominator;
 
-            // Calculate the wheel powers
-            double frontLeftPower = y + x + z;
-            double frontRightPower = y - x - z;
-            double backLeftPower = y - x + z;
-            double backRightPower = y + x - z;
+    y = -gamepad1.left_stick_y;
+    x = gamepad1.left_stick_x;
+    rx = gamepad2.right_stick_x;
 
-            // Set the motor powers
-            frontLeftMotor.setPower(frontLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backLeftMotor.setPower(backLeftPower);
-            backRightMotor.setPower(backRightPower);
+    frontLeftMotor.setPower(y +);
 
-            // Display the telemetry data
-            telemetry.addData("Front Left Power", frontLeftPower);
-            telemetry.addData("Front Right Power", frontRightPower);
-            telemetry.addData("Back Left Power", backLeftPower);
-            telemetry.addData("Back Right Power", backRightPower);
-            telemetry.update();
-        }
-    }
-
-    // A helper method to apply the dead zone to a joystick value
-    private double applyDeadZone(double value, double deadZone) {
-        if (Math.abs(value) < deadZone) {
-            return 0.0;
-        } else {
-            return value;
-        }
-    }
 }
 

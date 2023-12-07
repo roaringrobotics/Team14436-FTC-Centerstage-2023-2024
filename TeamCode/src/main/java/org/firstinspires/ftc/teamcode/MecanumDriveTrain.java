d5982575723763da157741d7ea67249b8d24e5a7
timmyjr11 <timmyytjr@gmail.com>
2023-12-07 12:04:55 -0700

    Commented and advsied on code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
@@ -30,36 +30,59 @@ public class MecanumDriveTrain extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         BlackLift = hardwareMap.get(DcMotor.class, "Blacklift");
 
+        // TODO: Reverse the Bluelift motor so the motors goes the same way.
+        //  this is so you don't mess up 1 and -1 and break the lift.
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        // TODO: Add "*motorName*.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // Without brake enabled, the robot could in theory not stay in the position you want it to.
+
         IMU imu = hardwareMap.get(IMU.class, "imu");
 
+        // I didn't know that these were the parameters for the IMU, so that's cool.
+        // Use this from now on lmao.
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
                 RevHubOrientationOnRobot.UsbFacingDirection.UP));
+
         imu.initialize(parameters);
 
 
         waitForStart();
+
+        // TODO: You forgot "isStopRequested()"
+        //  Is is more to make sure the code actually stops when requested
         while (opModeIsActive()) {
+
+            // TODO: TBH, I would have put this into its own method,
+            //  there should be a code called " " that goes over it.
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
+            // This is an entirely new thing this year, this was never how you were supposed to get
+            // The angles. Roadrunner is gonna be need to be changed if it doesn't update.
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
+            // TODO: Everything from here to line 73 is useless as you don't even use calculated powers
+            //  to drive the thing. I would either remove it or use the powers for the motors
             double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
             double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
             rotX = rotX * 1.1;
 
             double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
 
+            // Like you don't even use the values here, and you're creating a new variable everytime
+            // the code loops.
+            // TODO: Declare the motor powers where you declare everything else and remove double
+            //  EX: in line 21 you put "double frontLeftPower" and then here you just put "frontLeftPower"
             double frontLeftPower = (rotY + rotX + rx) / denominator;
             double frontRightPower = (rotY - rotX - rx) / denominator;
             double backLeftPower = (rotY - rotX + rx) / denominator;
             double backRightPower = (rotY + rotX - rx) / denominator;
 
+            // TODO: You repeated lines 55 - 57. There is no need to recall the statement here.
             y = -gamepad1.left_stick_y;
             x = gamepad1.left_stick_x;
             rx = gamepad1.right_stick_x;
@@ -69,12 +92,22 @@ public class MecanumDriveTrain extends LinearOpMode {
             frontRight.setPower(y - x - rx);
             backRight.setPower(y + x - rx);
 
+            // I cleaned up the code a little bit, but I didn't change anything here.
+            // TODO: Change gamepad1 to gamepad2. Lines 96 and all the way down.
+            // This is written so only one person drives the robot and does everything on it.
+            // The second player (who does the lift) cannot do anything because you didn't set it as such
             if (gamepad1.a){
                 blueIntake.setPower(1);
             }
             else {
                 blueIntake.setPower(0);
             }
+
+            // TODO: Make this an if, if else, else statement. Writing it like this could break the bot!
+            // Because it's not a singular statement. The robot will fight itself.
+            // For example: If you hold up on d-pad, the robot lift motors will swap between 0 and 1
+            // This is because you set the lift motors to 0 because dPad down isn't being pressed,
+            // no matter if dPad up is pressed.
 
             if(gamepad1.dpad_down){
                 Bluelift.setPower(1);
@@ -92,12 +125,6 @@ public class MecanumDriveTrain extends LinearOpMode {
                 Bluelift.setPower(0);
                 BlackLift.setPower(0);
             }
-
-
-
-
-
         }
-
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrainEXTIM.java
@@ -0,0 +1,96 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+/* Here is the simplified version of your code in the way that I would write it. Now I don't want you
+    to copy it without thinking, but rather try to understand why I wrote the code in the way that I do.
+    This removes all of the lines of code that were useless and only made the robot slower.
+    This program also features two important principles of Java programming: Encapsulation and abstraction
+    from the "motor" method.
+    */
+public class MecanumDriveTrainEXTIM extends LinearOpMode {
+    /* I decided to combine all of the drive motors, lift motors, and intake motor together into
+        their own statement, as it is both clean and easy to read.
+        Notice I also used DcMotorEx and not DcMotor.
+        This is so you can use more advanced features such as getting velocities and such
+        While it is not needed, I think by having the methods available to you will improve your understanding
+        of what you can do with motors.
+     */
+    DcMotorEx backLeft, frontLeft, frontRight, backRight;
+    DcMotorEx Bluelift, BlackLift;
+    DcMotorEx blueIntake;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        blueIntake = hardwareMap.get(DcMotorEx.class, "blueIntake");
+        Bluelift = hardwareMap.get(DcMotorEx.class, "Bluelift");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        BlackLift = hardwareMap.get(DcMotorEx.class, "Blacklift");
+
+        // I reversed the Bluelift so motor powers are the same.
+        // This is so messing up negative values is harder and
+        // by extension not mess up the lift.
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        Bluelift.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+
+            /* This single line of code physically does the same thing as your 19 lines of code.
+                This method allows the driver to actually drive the robot.
+                Just as I have stated in around line 80, the parameters x, y, and rx are then put in as
+                the stick inputs. Think of the x, y, and rx as abstract variables in math and you are now
+                putting values to those variables.
+            */
+            motor(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+            if (gamepad2.a){
+                blueIntake.setPower(1);
+            } else {
+                blueIntake.setPower(0);
+            }
+
+            // This uses the if, if else, else statement
+            // This is so only one of these statements will set the power depending on what you press
+            if(gamepad2.dpad_down){
+                Bluelift.setPower(-1);
+                BlackLift.setPower(-1);
+            } else if (gamepad2.dpad_up) {
+                Bluelift.setPower(1);
+                BlackLift.setPower(1);
+            } else {
+                Bluelift.setPower(0);
+                BlackLift.setPower(0);
+            }
+        }
+    }
+
+    /* This is a method. Methods are used to basically contain your code into different sections.
+        This is important for troubleshooting; if one method breaks then you know what section it is in.
+        Moreover, it makes your code just look so much nicer and cool. Plus, you can call this whenever
+        you need it, rather than rewrite everything again.
+
+        This method, motor, has the parameters x, y, and rx. We use these parameters to calculate
+        the motor powers.
+
+        While this is robot-centric, your code was already written in a robot-centric way soooo.
+        Plus, this is way faster, as you only call the powers once per cycle, rather than call them,
+        do math that doesn't help the robot, and call the same powers again and then assign their power
+        to the motors.
+     */
+
+    private void motor(double x, double y, double rx) {
+        frontLeft.setPower(y + x + rx);
+        backLeft.setPower(y - x + rx);
+        frontRight.setPower(y - x - rx);
+        backRight.setPower(y + x - rx);
+    }
+}

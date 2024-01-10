4e2d3ed817179e339211cf7f73222b9b0b42f97b
roaringrobotics <roaringrobotics14436@gmail.com>
2024-01-09 17:53:38 -0700

    Working on Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
@@ -1,14 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.google.blocks.ftcrobotcontroller.runtime.ImuAccess;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.Servo;
 
-import org.apache.commons.math3.analysis.function.Power;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 import java.util.ArrayList;
@@ -43,11 +42,12 @@ public class MecanumDriveTrain extends LinearOpMode {
 
     double slow;
 
-    //static slide slide=MecanumDriveTrain.slide.collapsed;
+    static extended slide=MecanumDriveTrain.slide.collapsed;
 
     ArrayList<Boolean> booleanArray = new ArrayList<>();
     int booleanIncrementer = 0;
     boolean y2Pressed;
+    private ImuAccess imu;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -166,28 +166,29 @@ public class MecanumDriveTrain extends LinearOpMode {
             slow = 0;
         }
 
-        rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-        rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+        botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
         rotX = rotX * 1.1;
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
 
-        denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-
-        frontLeftPower = (rotY + rotX + rx) / denominator;
-        frontRightPower = (rotY - rotX - rx) / denominator;
-        backLeftPower = (rotY - rotX + rx) / denominator;
-        backRightPower = (rotY + rotX - rx) / denominator;
-
-
-        frontLeft.setPower(frontLeftPower );
-        backLeft.setPower(backLeftPower );
-        frontRight.setPower(frontRightPower );
+        frontLeft.setPower(frontLeftPower);
+        backLeft.setPower(backLeftPower);
+        frontRight.setPower(frontRightPower);
         backRight.setPower(backRightPower);
 
     }
 
-    // LIST: [true, ]
+     LIST: [true, ]
 
-      /*  private boolean ifPressed(boolean button) {
+       private boolean ifPressed(boolean button) {
             boolean output = false;
 
             if (booleanArray.size() == booleanIncrementer) {
@@ -207,8 +208,11 @@ public class MecanumDriveTrain extends LinearOpMode {
             return output;
         }
         private enum slide{
-*/
-        // add extended
-        //collapsed,tip
-        //}
+         add extended
+        collapsed,tip
+        }
+
+    private static class extended {
+        public extended collapsed;
+    }
 }
